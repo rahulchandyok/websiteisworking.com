aa386e0fadacdea21165fe2f14194644804a071f
Tavishi Jain <tavishi@bluestacks.com>
2019-09-30 19:44:14 +0530

    changing string logic

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ function getResponse() {
     if (this.readyState == 4 && this.status == 200) {
       let response = JSON.parse(this.responseText)
       console.log(response)
-      if (response['statusCode'] != 200) {
-        let errorResponse = response['returnedUrl'] + " " + response['statusText']
-        document.getElementById("error").innerHTML = errorResponse
-      }
-      else {
-        let successResponse = response['returnedUrl'] + " is " + (response['isDown'] ? "not working" : "working")
-        console.log(successResponse)
-        document.getElementById("success").innerHTML = successResponse
-      }
+      document.getElementById("success").innerHTML = response['returnedUrl'] + " is " + (response['isDown'] ? "not working" : "working")
     }
   }
   xhttp.open("GET", "https://api.downfor.cloud/httpcheck/" + website, true)
