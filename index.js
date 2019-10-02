3f82bfcd95ea5b06aefd6d9daab81f27b34a335c
malkeet-bst <malkeet@bluestacks.com>
2019-10-02 16:39:57 +0530

    design changes

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-function getResponse() {
-  let website = document.getElementById("domain").value
-  console.log(website)
-  let xhttp = new XMLHttpRequest()
-  xhttp.onreadystatechange = function () {
+function getResponse(e) {
+  let website = document.getElementById('domain').value;
+  if (!website) return;
+
+  fetchHistory(website);
+
+  document.getElementById('loader').style.display = 'block';
+  document.getElementById('response').style.display = 'none';
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      let response = JSON.parse(this.responseText)
-      console.log(response)
-      document.getElementById("success").innerHTML = response['returnedUrl'] + " is " + (response['isDown'] ? "not working" : "working")
+      let response = JSON.parse(this.responseText);
+      console.log(response);
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('response').style.display = 'block';
+      if (response['isDown']) {
+        document.getElementById('website-error').innerHTML =
+          response['returnedUrl'];
+        document.getElementById('success').style.display = 'none';
+        document.getElementById('error').style.display = 'block';
+      } else {
+        document.getElementById('website-success').innerHTML =
+          response['returnedUrl'];
+        document.getElementById('success').style.display = 'block';
+        document.getElementById('error').style.display = 'none';
+      }
     }
-  }
-  xhttp.open("GET", "https://api.downfor.cloud/httpcheck/" + website, true)
-  xhttp.send()
+  };
+  xhttp.open('GET', 'https://api.downfor.cloud/httpcheck/' + website, true);
+  xhttp.send();
+}
+function fetchHistory(website) {
+  let recentSearches = JSON.parse(localStorage.getItem('recentSearches'));
+  if (!recentSearches) recentSearches = '';
+  if (website) recentSearches = website + '<br>' + recentSearches;
+  localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
+  document.getElementById('recent').innerHTML = recentSearches;
 }
