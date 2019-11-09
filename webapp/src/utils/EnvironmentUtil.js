f76e2300c34fe68b952f21af938a6b4712efeb1c
Tavishi Jain <tavishi@bluestacks.com>
2019-11-09 19:45:33 +0530

    adding a log

--- webapp/src/utils/EnvironmentUtil.js
+++ webapp/src/utils/EnvironmentUtil.js
@@ -1,6 +1,7 @@
 const EnvironmentUtil = {
   getApiUrl: () => {
     let hostname = window.location.hostname
+    console.log(hostname)
     if (hostname === 'localhost')
       return 'http://localhost:8080'
     else
